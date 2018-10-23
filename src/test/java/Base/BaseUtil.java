211ea74a956c4210be836edb7b496286dea9b0a1
Lisa <lcoppola@users.noreply.github.com>
2018-10-23 13:59:59 -0600

    Update BaseUtil.java

--- src/test/java/Base/BaseUtil.java
+++ src/test/java/Base/BaseUtil.java
@@ -1,12 +1,13 @@
 package Base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Created by Karthik on 10/21/2016.
  */
 public class BaseUtil {
 
-    public WebDriver Driver;
+    public RemoteWebDriver Driver;
 
 }
