cbbc065ce075ddd1174a3b18532d260e00af0d91
Lisa <lcoppola@users.noreply.github.com>
2018-10-23 14:19:37 -0600

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -7,6 +7,7 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.*;
 
 /**
  * Created by Karthik on 10/17/2016.
@@ -35,7 +36,7 @@ public class Hook extends BaseUtil{
         //base.Driver = new ChromeDriver();
          
         //WebDriver driver = new RemoteWebDriver(new URL("http://dbg-trp-jenkins.swg-devops.com:4444/wd/hub"), capability);
-        base.Driver = new RemoteWebDriver(new URL("http://dbg-trp-jenkins.swg-devops.com:4444/wd/hub"), capability);
+        base.Driver = new RemoteWebDriver(new URL("http://dbg-trp-jenkins.swg-devops.com:4444/wd/hub"), Capability.getBrowserName());
     }
 
 
