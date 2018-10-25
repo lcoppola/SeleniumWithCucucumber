4154e57c40d8fc85d2fb5c305d9cf47cc24d68b7
Lisa <lcoppola@users.noreply.github.com>
2018-10-25 15:14:11 -0600

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -3,7 +3,9 @@ package steps;
 import Base.BaseUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import cucumber.api.java.Before; 
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -29,8 +31,20 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
-        base.Driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+       // base.Driver = new ChromeDriver();
+        
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                    .usingDriverExecutable(new File("/var/jenkins_home/tools/chromedriver/chromedriver"))
+                    .usingAnyFreePort()
+                    .build();
+            service.start();
+            try {
+                service.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            base.Driver = new RemoteWebDriver(service.getUrl(),DesiredCapabilities.chrome());
     }
 
 
